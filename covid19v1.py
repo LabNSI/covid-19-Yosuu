1700b24ba0c9392c7603c8131dfa20b7e5c337fd
Yosuu <90187721+Yosuu@users.noreply.github.com>
2022-01-10 10:50:28 +0000

    Solution v1

--- covid19v1.py
+++ covid19v1.py
@@ -12,23 +12,21 @@
 import urllib.request as urllib2
 
 def download_file(url, file_name):
-    '''
-    Download a file from url and copy it localy with file_name as name
-    '''
+  
     try:
         # open url passed in argument
-        file = urllib2.urlopen(___)
+        file = urllib2.urlopen(url)
         # open file for writing in binary mode
         # https://docs.python.org/3/library/functions.html#open
-        with open(___, '___') as output:
+        with open(file_name, 'wb') as output:
             # write in output the file read
             # use write and read functions
-            output.___(file.___())
-        # OK : return true
-        return ___
+            output.write(file.read())
+        
+        return True
     except:
-        # Something is wrong : return false
-        return ___
+        
+        return False
      
 # here the main code
 if __name__ == '__main__':
@@ -36,7 +34,7 @@ if __name__ == '__main__':
     # File name of datas
     file = "time_series_19-covid-Confirmed.csv"
     # where datas are located
-    url = ___
+    url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 
     if download_file(url,file):
         print(f'Téléchargement du fichier {file} terminé avec succès')
